0e257931d7026a2452ea0d87affba2257b868272
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 19:31:52 +0100

    Update __init__.py

--- components/ftp_client/__init__.py
+++ components/ftp_client/__init__.py
@@ -1,45 +1,64 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import storage
+from esphome.const import CONF_ID
 
-# Define constants explicitly since import from esphome.const failed
-CONF_ID = 'id'
-CONF_SERVER = 'server'
-CONF_USERNAME = 'username'
-CONF_PASSWORD = 'password'
-CONF_PORT = 'port'
-CONF_MODE = 'mode'
-MULTI_CONF = True
+DEPENDENCIES = ['network']
+AUTO_LOAD = ['ftp_client']
 
-ftp_client_ns = cg.esphome_ns.namespace('ftp_client')
-FTPClient = ftp_client_ns.class_('FTPClient', cg.Component)
+CONF_FTP_SERVER = 'server'
+CONF_FTP_USERNAME = 'username'
+CONF_FTP_PASSWORD = 'password'
+CONF_FTP_PORT = 'port'
+CONF_FTP_MODE = 'mode'
+CONF_FTP_FILES = 'files'
 
-# Enum for FTP modes
+ftp_storage_ns = cg.esphome_ns.namespace('ftp_storage')
+FTPStorage = ftp_storage_ns.class_('FTPStorage', storage.Storage, cg.Component)
+
 FTP_MODES = {
-    'ACTIVE': ftp_client_ns.FTPMode.ACTIVE,
-    'PASSIVE': ftp_client_ns.FTPMode.PASSIVE
+    'ACTIVE': ftp_storage_ns.FTPMode.ACTIVE,
+    'PASSIVE': ftp_storage_ns.FTPMode.PASSIVE
 }
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.Required(CONF_ID): cv.declare_id(FTPClient),
-    cv.Required(CONF_SERVER): cv.string,
-    cv.Optional(CONF_PORT, default=21): cv.port,
-    cv.Required(CONF_USERNAME): cv.string,
-    cv.Required(CONF_PASSWORD): cv.string,
-    cv.Optional(CONF_MODE, default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
-    cv.Optional('buffer_size', default=1024): cv.int_range(min=128, max=8192),
-    cv.Optional('timeout', default=30000): cv.int_range(min=1000, max=60000),
+def validate_ftp_file(value):
+    if not isinstance(value, dict):
+        raise cv.Invalid("File must be a dictionary")
+    
+    if 'source' not in value:
+        raise cv.Invalid("File must have 'source' key")
+    
+    if not value['source'].startswith('ftp://'):
+        raise cv.Invalid("Source must be an FTP URL (ftp://...)")
+    
+    if 'id' not in value:
+        raise cv.Invalid("File must have 'id' key")
+    
+    return value
+
+CONFIG_SCHEMA = storage.STORAGE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(FTPStorage),
+    cv.Required(CONF_FTP_SERVER): cv.string,
+    cv.Optional(CONF_FTP_PORT, default=21): cv.port,
+    cv.Required(CONF_FTP_USERNAME): cv.string,
+    cv.Required(CONF_FTP_PASSWORD): cv.string,
+    cv.Optional(CONF_FTP_MODE, default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
+    cv.Required(CONF_FTP_FILES): cv.ensure_list(validate_ftp_file),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    cg.add(var.set_server(config[CONF_SERVER]))
-    cg.add(var.set_port(config[CONF_PORT]))
-    cg.add(var.set_username(config[CONF_USERNAME]))
-    cg.add(var.set_password(config[CONF_PASSWORD]))
-    cg.add(var.set_mode(config[CONF_MODE]))
-    cg.add(var.set_transfer_buffer_size(config['buffer_size']))
-    cg.add(var.set_timeout_ms(config['timeout']))
+    cg.add(var.set_server(config[CONF_FTP_SERVER]))
+    cg.add(var.set_port(config[CONF_FTP_PORT]))
+    cg.add(var.set_username(config[CONF_FTP_USERNAME]))
+    cg.add(var.set_password(config[CONF_FTP_PASSWORD]))
+    cg.add(var.set_mode(config[CONF_FTP_MODE]))
     
-    return var
+    for file_config in config[CONF_FTP_FILES]:
+        source = file_config['source']
+        id = file_config['id']
+        cg.add(var.add_file(source, id))
+    
+    await storage.register_storage(var, config)
