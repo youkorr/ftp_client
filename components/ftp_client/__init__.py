b5b7d330e395bf2c70c747b08e1c7618b967f005
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 19:40:38 +0100

    Update __init__.py

--- components/ftp_client/__init__.py
+++ components/ftp_client/__init__.py
@@ -10,34 +10,24 @@ CONF_MODE = 'mode'
 CONF_BUFFER_SIZE = 'buffer_size'
 CONF_TIMEOUT = 'timeout'
 CONF_FILES = 'files'
-CONF_REMOTE_PATH = 'remote_path'
+CONF_SOURCE = 'source'
 CONF_LOCAL_PATH = 'local_path'
-CONF_FILE_ID = 'id'
 
 ftp_client_ns = cg.esphome_ns.namespace('ftp_client')
-FTPClientComponent = ftp_client_ns.class_('FTPClientComponent', cg.Component)
-
-ftp_storage_ns = cg.esphome_ns.namespace('ftp_storage')
-FTPStorageComponent = ftp_storage_ns.class_('FTPStorageComponent', cg.Component)
+FTPClient = ftp_client_ns.class_('FTPClient', cg.Component)
 
 FTP_MODES = {
     'ACTIVE': ftp_client_ns.FTPMode.ACTIVE,
     'PASSIVE': ftp_client_ns.FTPMode.PASSIVE
 }
 
-def validate_ftp_file(value):
-    if not isinstance(value, dict):
-        raise cv.Invalid("File config must be a dictionary")
-    if CONF_REMOTE_PATH not in value:
-        raise cv.Invalid(f"File config must contain '{CONF_REMOTE_PATH}'")
-    if CONF_LOCAL_PATH not in value:
-        raise cv.Invalid(f"File config must contain '{CONF_LOCAL_PATH}'")
-    if CONF_FILE_ID not in value:
-        raise cv.Invalid(f"File config must contain '{CONF_FILE_ID}'")
+def validate_ftp_url(value):
+    if not value.startswith('ftp://'):
+        raise cv.Invalid("URL must start with ftp://")
     return value
 
-FTP_CLIENT_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(FTPClientComponent),
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(FTPClient),
     cv.Required(CONF_SERVER): cv.string,
     cv.Optional(CONF_PORT, default=21): cv.port,
     cv.Required(CONF_USERNAME): cv.string,
@@ -45,47 +35,30 @@ FTP_CLIENT_SCHEMA = cv.Schema({
     cv.Optional(CONF_MODE, default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
     cv.Optional(CONF_BUFFER_SIZE, default=2048): cv.int_range(min=512, max=8192),
     cv.Optional(CONF_TIMEOUT, default=30000): cv.int_range(min=1000, max=60000),
-})
-
-FTP_STORAGE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(FTPStorageComponent),
-    cv.Required(CONF_SERVER): cv.string,
-    cv.Required(CONF_USERNAME): cv.string,
-    cv.Required(CONF_PASSWORD): cv.string,
-    cv.Required(CONF_FILES): cv.ensure_list(validate_ftp_file),
-})
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.Optional('ftp_client'): FTP_CLIENT_SCHEMA,
-    cv.Optional('ftp_storage'): FTP_STORAGE_SCHEMA,
-})
+    cv.Optional(CONF_FILES, default=[]): cv.ensure_list(
+        cv.Schema({
+            cv.Required(CONF_SOURCE): validate_ftp_url,
+            cv.Required(CONF_ID): cv.string,
+            cv.Optional(CONF_LOCAL_PATH): cv.string,
+        })
+    ),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    if 'ftp_client' in config:
-        ftp_config = config['ftp_client']
-        var = cg.new_Pvariable(ftp_config[CONF_ID])
-        await cg.register_component(var, ftp_config)
-        
-        cg.add(var.set_server(ftp_config[CONF_SERVER]))
-        cg.add(var.set_port(ftp_config[CONF_PORT]))
-        cg.add(var.set_username(ftp_config[CONF_USERNAME]))
-        cg.add(var.set_password(ftp_config[CONF_PASSWORD]))
-        cg.add(var.set_mode(ftp_config[CONF_MODE]))
-        cg.add(var.set_transfer_buffer_size(ftp_config[CONF_BUFFER_SIZE]))
-        cg.add(var.set_timeout_ms(ftp_config[CONF_TIMEOUT]))
-
-    if 'ftp_storage' in config:
-        storage_config = config['ftp_storage']
-        storage = cg.new_Pvariable(storage_config[CONF_ID])
-        await cg.register_component(storage, storage_config)
-        
-        cg.add(storage.set_server(storage_config[CONF_SERVER]))
-        cg.add(storage.set_username(storage_config[CONF_USERNAME]))
-        cg.add(storage.set_password(storage_config[CONF_PASSWORD]))
-        
-        for file in storage_config[CONF_FILES]:
-            cg.add(storage.add_file(
-                file[CONF_REMOTE_PATH],
-                file[CONF_LOCAL_PATH],
-                file[CONF_FILE_ID]
-            ))
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    cg.add(var.set_server(config[CONF_SERVER]))
+    cg.add(var.set_port(config[CONF_PORT]))
+    cg.add(var.set_username(config[CONF_USERNAME]))
+    cg.add(var.set_password(config[CONF_PASSWORD]))
+    cg.add(var.set_mode(config[CONF_MODE]))
+    cg.add(var.set_transfer_buffer_size(config[CONF_BUFFER_SIZE]))
+    cg.add(var.set_timeout_ms(config[CONF_TIMEOUT]))
+    
+    for file in config[CONF_FILES]:
+        cg.add(var.add_file(
+            file[CONF_SOURCE],
+            file[CONF_ID],
+            file.get(CONF_LOCAL_PATH, "")
+        ))
