9e238dd315808f4717ad41677137e7d2f6184d27
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 19:30:12 +0100

    Update __init__.py

--- components/ftp_client/__init__.py
+++ components/ftp_client/__init__.py
@@ -11,7 +11,6 @@ CONF_MODE = 'mode'
 MULTI_CONF = True
 
 ftp_client_ns = cg.esphome_ns.namespace('ftp_client')
-# Remove inheritance from StorageSource if it's no longer available
 FTPClient = ftp_client_ns.class_('FTPClient', cg.Component)
 
 # Enum for FTP modes
@@ -20,47 +19,26 @@ FTP_MODES = {
     'PASSIVE': ftp_client_ns.FTPMode.PASSIVE
 }
 
-def validate_config(config):
-    """Additional validation for FTP configuration"""
-    # Validate username
-    if len(config.get(CONF_USERNAME, '')) < 1:
-        raise cv.Invalid("Username must not be empty")
-    
-    # Validate password
-    if len(config.get(CONF_PASSWORD, '')) < 1:
-        raise cv.Invalid("Password must not be empty")
-    
-    return config
-
-# Create the base schema first
-BASE_CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = cv.Schema({
+    cv.Required(CONF_ID): cv.declare_id(FTPClient),
     cv.Required(CONF_SERVER): cv.string,
     cv.Optional(CONF_PORT, default=21): cv.port,
     cv.Required(CONF_USERNAME): cv.string,
     cv.Required(CONF_PASSWORD): cv.string,
-    cv.Required(CONF_ID): cv.declare_id(FTPClient),
-    
-    # Configuration options
-    cv.Optional('mode', default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
+    cv.Optional(CONF_MODE, default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
     cv.Optional('buffer_size', default=1024): cv.int_range(min=128, max=8192),
     cv.Optional('timeout', default=30000): cv.int_range(min=1000, max=60000),
-})
-
-# Create the final configuration schema by applying the validation function
-CONFIG_SCHEMA = BASE_CONFIG_SCHEMA.extend(validate_config)
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # Set basic connection parameters
     cg.add(var.set_server(config[CONF_SERVER]))
     cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_username(config[CONF_USERNAME]))
     cg.add(var.set_password(config[CONF_PASSWORD]))
-    
-    # Set additional configuration
-    cg.add(var.set_mode(config['mode']))
+    cg.add(var.set_mode(config[CONF_MODE]))
     cg.add(var.set_transfer_buffer_size(config['buffer_size']))
     cg.add(var.set_timeout_ms(config['timeout']))
     
