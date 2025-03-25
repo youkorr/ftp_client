49bed536850857d869994fb413330df4bf4c8b3c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-25 19:43:04 +0100

    Update __init__.py

--- components/ftp_client/__init__.py
+++ components/ftp_client/__init__.py
@@ -1,30 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PASSWORD, CONF_PORT, CONF_USERNAME
+from esphome import automation
+from esphome.components import switch
+from esphome.const import CONF_ID, CONF_PASSWORD, CONF_PORT, CONF_SERVER, CONF_USERNAME
 
-DEPENDENCIES = ['network']
-CODEOWNERS = ['@your-github-username']
+CODEOWNERS = ["@your_github_username"]
 
-CONF_SERVER = 'server'
-CONF_MODE = 'mode'
-CONF_BUFFER_SIZE = 'buffer_size'
-CONF_TIMEOUT = 'timeout'
+ftp_ns = cg.esphome_ns.namespace('ftp')
+FTPClient = ftp_ns.class_('FTPClient', cg.Component)
+FTPDownloadSwitch = ftp_ns.class_('FTPDownloadSwitch', switch.Switch, cg.Component)
+
 CONF_FILES = 'files'
 CONF_SOURCE = 'source'
-CONF_LOCAL_PATH = 'local_path'
+CONF_FILE_ID = 'id'
 
-ftp_client_ns = cg.esphome_ns.namespace('ftp_client')
-FTPClient = ftp_client_ns.class_('FTPClient', cg.Component)
-
-FTP_MODES = {
-    'ACTIVE': ftp_client_ns.FTPMode.ACTIVE,
-    'PASSIVE': ftp_client_ns.FTPMode.PASSIVE
-}
-
-def validate_ftp_url(value):
-    if not value.startswith('ftp://'):
-        raise cv.Invalid("URL must start with ftp://")
-    return value
+FTP_FILE_SCHEMA = cv.Schema({
+    cv.Required(CONF_SOURCE): cv.string,
+    cv.Required(CONF_FILE_ID): cv.string,
+})
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FTPClient),
@@ -32,17 +25,8 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_PORT, default=21): cv.port,
     cv.Required(CONF_USERNAME): cv.string,
     cv.Required(CONF_PASSWORD): cv.string,
-    cv.Optional(CONF_MODE, default='PASSIVE'): cv.enum(FTP_MODES, upper=True),
-    cv.Optional(CONF_BUFFER_SIZE, default=2048): cv.int_range(min=512, max=8192),
-    cv.Optional(CONF_TIMEOUT, default=30000): cv.int_range(min=1000, max=60000),
-    cv.Optional(CONF_FILES, default=[]): cv.ensure_list(
-        cv.Schema({
-            cv.Required(CONF_SOURCE): validate_ftp_url,
-            cv.Required(CONF_ID): cv.string,
-            cv.Optional(CONF_LOCAL_PATH): cv.string,
-        })
-    ),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Optional(CONF_FILES): cv.ensure_list(FTP_FILE_SCHEMA),
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -52,13 +36,10 @@ async def to_code(config):
     cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_username(config[CONF_USERNAME]))
     cg.add(var.set_password(config[CONF_PASSWORD]))
-    cg.add(var.set_mode(config[CONF_MODE]))
-    cg.add(var.set_transfer_buffer_size(config[CONF_BUFFER_SIZE]))
-    cg.add(var.set_timeout_ms(config[CONF_TIMEOUT]))
     
-    for file in config[CONF_FILES]:
-        cg.add(var.add_file(
-            file[CONF_SOURCE],
-            file[CONF_ID],
-            file.get(CONF_LOCAL_PATH, "")
-        ))
+    if CONF_FILES in config:
+        for file_config in config[CONF_FILES]:
+            source = file_config[CONF_SOURCE]
+            file_id = file_config[CONF_FILE_ID]
+            cg.add(var.add_file(source, file_id))
+
